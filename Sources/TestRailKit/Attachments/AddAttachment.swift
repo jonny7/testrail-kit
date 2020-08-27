0f6800ad08d7a48fcd3fe458b9ea64dc8dcfee65
Jonathon <jonathon.cooper7@gmail.com>
2020-08-27 20:52:45 -0300

    completes addAttachment routes

--- Sources/TestRailKit/Attachments/AddAttachment.swift
+++ Sources/TestRailKit/Attachments/AddAttachment.swift
@@ -11,12 +11,25 @@ public enum AddAttachment {
     ///   - entryId: The ID of the test plan entry the attachment should be added to
     case toTestPlanEntry(planId: Int, entryId: Int)
     
+    /// Adds attachment to a result based on the result ID. The maximum allowable upload size is set to 256mb. Requires TestRail 5.7 or later
+    /// Please Note: The ability to edit test results must be enabled under ‘Site Settings’ in order for add_attachment_to_result endpoints to work.
+    /// - Parameter resultId: The ID of the test result the attachment should be added to
+    case toResult(resultId: Int)
+    
+    /// Adds attachment to test run. The maximum allowable upload size is set to 256mb. Requires TestRail 6.3 or later
+    /// - Parameter runId: The ID of the test run the attachment should be added to
+    case toRun(rundId: Int)
+    
     var uri: String {
         switch self {
         case .toPlan(let planId):
             return "add_attachment_to_plan/\(planId)"
         case .toTestPlanEntry(let planId, let entryId):
             return "add_attachment_to_plan_entry/\(planId)/\(entryId)"
+        case .toResult(let resultId):
+            return "add_attachment_to_result/\(resultId)"
+        case .toRun(let runId):
+            return "add_attachment_to_run/\(runId)"
         }
     }
 }
