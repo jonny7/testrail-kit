39ba381cab4dd99d09299dbec2b04353610c35af
Jonathon <jonathon.cooper7@gmail.com>
2020-08-28 13:48:03 -0300

    completes refactor for Attachment Routes

---
+++ Sources/TestRailKit/Attachments/AttachmentData.swift
@@ -0,0 +1,21 @@
+import NIOHTTP1
+
+public enum AttachmentData {
+    /// Retrieves the requested file identified by :attachment_id. Requires TestRail 5.7 or later
+    /// - Parameter attachmentId: The ID of the attachment to retrieve
+    case get(attachmentId: Int)
+    
+    /// Deletes the specified attachment identified by :attachment_id. Requires TestRail 5.7 or later
+    /// - Parameter attachmentId: The ID of the attachment to to delete
+    case delete(attachmentId: Int)
+    
+    var request: (String, HTTPMethod) {
+        switch self {
+        case .get(let attachmentId):
+            return ("get_attachment/\(attachmentId)", .GET)
+        case .delete(let attachmentId):
+            return ("delete_attachment/\(attachmentId)", .POST)
+        }
+    }
+    
+}
---
+++ Sources/TestRailKit/Attachments/TestRailAttachments.swift
@@ -0,0 +1,3 @@
+public struct TestRailAttachments: TestRailModel {
+    public var data: [TestRailAttachment]
+}
