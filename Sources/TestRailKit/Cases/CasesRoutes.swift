f8245021dc198fe6b8eee99792f75f6551d28fcb
Jonny <6732997+jonny7@users.noreply.github.com>
2020-09-09 09:00:42 -0300

    completes cases

--- Sources/TestRailKit/Cases/CasesRoutes.swift
+++ Sources/TestRailKit/Cases/CasesRoutes.swift
@@ -10,13 +10,16 @@ public protocol CaseRoutes {
     ///   - id: The `caseId` for updating a case or the `sectionId` of where the case should be added
     ///   - testCase: `Codable` test case
     func addCase<TM: TestRailModel>(type: Case, id: Int, testCase: TM) throws -> EventLoopFuture<TestRailCase>
-
+    
+    /// This generic method allows you to retrieve either a single case or multiple
+    /// See https://www.gurock.com/testrail/docs/api/reference/cases#get_case and https://www.gurock.com/testrail/docs/api/reference/cases#get_cases
+    /// - Parameter type: See `Case`
     func getCase<TM: TestRailModel>(type: Case) -> EventLoopFuture<TM>
 
     /// Deletes an existing test case.
     /// Looks as this returns an empty body and response of 200. This really should be 204 imo
     /// - Parameter caseId: The ID of the test case
-    //func deleteCase(caseId: Int) -> EventLoopFuture<TestRailDataResponse>
+    func deleteCase(caseId: Int) -> EventLoopFuture<TestRailDataResponse>
     
     /// Headers to send with the request.
     var headers: HTTPHeaders { get set }
@@ -32,17 +35,17 @@ public struct TestRailCaseRoutes: CaseRoutes {
         self.apiHandler = apiHandler
     }
     
-    public func addCase<TM>(type: Case, id: Int, testCase: TM) throws -> EventLoopFuture<TestRailCase> where TM : TestRailModel {
+    public func addCase<TM>(type: Case, id: Int, testCase: TM) throws -> EventLoopFuture<TestRailCase> where TM: TestRailModel {
         let body = try encodeTestRailModel(data: testCase)
         return apiHandler.send(method: .POST, path: "\(type.uri.0)\(id)", body: .string(body), headers: headers)
     }
     
-    public func getCase<TM>(type: Case) -> EventLoopFuture<TM> where TM : TestRailModel {
+    public func getCase<TM>(type: Case) -> EventLoopFuture<TM> where TM: TestRailModel {
         let filter = type.uri.1 ?? ""
         return apiHandler.send(method: .GET, path: "\(type.uri.0)", query: filter, headers: headers)
     }
 
-//    public func deleteCase(caseId: Int) -> EventLoopFuture<TestRailDataResponse> {
-//        return apiHandler.send(method: .POST, path: "delete_case/\(caseId)", headers: headers)
-//    }
+    public func deleteCase(caseId: Int) -> EventLoopFuture<TestRailDataResponse> {
+        return apiHandler.send(method: .POST, path: "delete_case/\(caseId)", headers: headers)
+    }
 }
