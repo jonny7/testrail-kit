7d623e7cd85d34867ffad103d85afb4dc64e420b
Jonny <6732997+jonny7@users.noreply.github.com>
2020-09-28 21:06:41 -0300

    case types and public access for some missing types

--- Sources/TestRailKit/Case Fields/AddedTestRailCaseField.swift
+++ Sources/TestRailKit/Case Fields/AddedTestRailCaseField.swift
@@ -1,20 +1,20 @@
 public struct AddedTestRailCaseField: CaseField, AddedCaseField {
-    var entityId: Int
-    var locationId: Int
-    var isMulti: Int
-    var statusId: Int
-    var isSystem: Int
-    var id: Int?
+    public var entityId: Int
+    public var locationId: Int
+    public var isMulti: Int
+    public var statusId: Int
+    public var isSystem: Int
+    public var id: Int?
     @BoolToIntCoder var isActive: Bool
-    var typeId: Int
-    var name: String
-    var systemName: String
-    var label: String
-    var description: String
-    var configs: String  // TestRail Changes from a object when using get, to a string when returning a succesffully added case_field
-    var displayOrder: Int
+    public var typeId: Int
+    public var name: String
+    public var systemName: String
+    public var label: String
+    public var description: String
+    public var configs: String  // TestRail Changes from a object when using get, to a string when returning a succesffully added case_field
+    public var displayOrder: Int
     @BoolToIntCoder var includeAll: Bool
-    var templateIds: [Int]
+    public var templateIds: [Int]
 }
 
 extension AddedTestRailCaseField: TestRailModel {}
--- Sources/TestRailKit/Case Fields/CaseFieldConfig.swift
+++ Sources/TestRailKit/Case Fields/CaseFieldConfig.swift
@@ -1,10 +1,10 @@
-struct CaseFieldConfig: Codable {
-    var context: CaseFieldContext
-    var options: CaseFieldOptions
-    var id: String?
+public struct CaseFieldConfig: Codable {
+    public var context: CaseFieldContext
+    public var options: CaseFieldOptions
+    public var id: String?
 }
 extension CaseFieldConfig {
-    init(from decoder: Decoder) throws {
+    public init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
         context = try container.decode(CaseFieldContext.self, forKey: .context)
         options = try container.decode(CaseFieldOptions.self, forKey: .options)
