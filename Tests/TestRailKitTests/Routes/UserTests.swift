e2177b77a1f325b9ba919b2c5687813f4ec24a8e
Jonathon <jonathon.cooper7@gmail.com>
2020-11-09 18:40:11 -0400

    adds extra user test

--- Tests/TestRailKitTests/Routes/UserTests.swift
+++ Tests/TestRailKitTests/Routes/UserTests.swift
@@ -149,4 +149,39 @@ class UserTests: XCTestCase {
         let response = try! requestComplete.wait()
         XCTAssertEqual(response.first?.email, "jonny@github.com")
     }
+    
+    func testGetUsersNonAdmin() {
+        var requestComplete: EventLoopFuture<[User]>!
+        XCTAssertNoThrow(
+            requestComplete = try Self.utilities.client.action(resource: UserResource.get(type: .all(projectId: 1)))
+        )
+
+        XCTAssertNoThrow(
+            XCTAssertEqual(
+                .head(
+                    .init(
+                        version: .init(major: 1, minor: 1),
+                        method: .GET,
+                        uri: "/index.php?/api/v2/get_users&project_id=1",
+                        headers: .init([
+                            ("authorization", "Basic dXNlckB0ZXN0cmFpbC5pbzoxMjM0YWJjZA=="),
+                            ("content-type", "application/json; charset=utf-8"),
+                            ("Host", "127.0.0.1:\(Self.utilities.testServer.serverPort)"),
+                            ("Content-Length", "0"),
+                        ]))),
+                try Self.utilities.testServer.readInbound()))
+
+        XCTAssertEqual(try Self.utilities.testServer.readInbound(), .end(nil))
+
+        var responseBuffer = Self.utilities.allocator.buffer(capacity: 0)
+        responseBuffer.writeString(Self.utilities.usersResponse)
+
+        XCTAssertNoThrow(
+            try Self.utilities.testServer.writeOutbound(.head(.init(version: .init(major: 1, minor: 1), status: .ok))))
+        XCTAssertNoThrow(try Self.utilities.testServer.writeOutbound(.body(.byteBuffer(responseBuffer))))
+        XCTAssertNoThrow(try Self.utilities.testServer.writeOutbound(.end(nil)))
+
+        let response = try! requestComplete.wait()
+        XCTAssertEqual(response.first?.email, "jonny@github.com")
+    }
 }
