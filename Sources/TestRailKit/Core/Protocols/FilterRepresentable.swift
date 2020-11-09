023e9fbdc7c6974d44aa889123af5d40781d6a53
Jonathon <jonathon.cooper7@gmail.com>
2020-11-09 14:54:55 -0400

    refactors IDRepresentable

--- Sources/TestRailKit/Core/Protocols/FilterRepresentable.swift
+++ Sources/TestRailKit/Core/Protocols/FilterRepresentable.swift
@@ -30,16 +30,6 @@ extension FilterRepresentable {
         return "\(limit)&offset=\(offset)"
     }
 
-    /// Default Implementation or string separated IDs
-    /// - Parameters:
-    ///   - name: query param
-    ///   - list: array of IDs
-    /// - Returns: TestRail comma separated list of IDs query param
-    func getIdList(name: String, list: [Int]) -> String {
-        let ids = list.map { String($0) }.joined(separator: ",")
-        return "&\(name)=\(ids)"
-    }
-
     /// Default Implementation of Timestamp
     /// - Parameters:
     ///   - name: query param
